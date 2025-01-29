be73d0f44b731ed5fc1047ccc87a13553332acb5
Sreejasirikonda22 <sirikondasreeja143@gmail.com>
2025-01-29 19:37:35 +0530

    Add files via upload

---
+++ GuessMyNumber.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+public class GuessMyNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numberToGuess = random.nextInt(10) + 1;
+        int numberOfTries = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Guess a number between 1 and 10: ");
+            int guess = scanner.nextInt();
+            numberOfTries++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low!");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Congratulations! You found the number in " + numberOfTries + " tries.");
+                break;
+            }
+        }
+    }
+}
